125ab7169b89eb07f4c24cd1777da4b28ddee665
nazmul <nazmul.apu.hasan@gmail.com>
2016-04-07 00:48:05 +0300

    log4j in use

---
+++ javatechnologies/src/homework6/Log4J.java
@@ -0,0 +1,18 @@
+package homework6;
+
+import org.apache.log4j.Logger;
+
+public class Log4J {
+	private static final Logger log = Logger.getLogger(Log4J.class);
+
+	public static void main(String[] args) {
+
+		try {
+			throw new Exception("New Exception");
+		} catch (Exception e) {
+			log.error(e, e);
+		}
+
+	}
+
+}
