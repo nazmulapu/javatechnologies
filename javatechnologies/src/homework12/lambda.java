927369b548fa531c6200aa732533e5ed500ffe1c
nazmul <nazmul.apu.hasan@gmail.com>
2016-05-20 00:13:16 +0300

    #lambda expressions with javafx

--- javatechnologies/src/homework12/lambda.java
+++ javatechnologies/src/homework12/lambda.java
@@ -1,19 +1,38 @@
 package homework12;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class lambda extends Application {
 	
 	public static void main(String[] args) {
+		launch(args);
 		
 	}
 
 	@Override
-	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
+	public void start(Stage stage) throws Exception {
+		Button button = new Button("Trigger ActionEvent...");
+		button.setOnAction(new DemoEventHandler());
+		stage.setScene(new Scene(button));
+		stage.show();
+	 }
+		
+	}
+	
+	class DemoEventHandler implements EventHandler <ActionEvent> {
+
+		@Override
+		public void handle(ActionEvent e) {
+			System.out.println(e.toString());
+			
+		}
 		
 	}
 
 	
-}
+
