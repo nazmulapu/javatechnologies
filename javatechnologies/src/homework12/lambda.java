b941304268b4c986c3da5c94bc24184c75ce018b
nazmul <nazmul.apu.hasan@gmail.com>
2016-05-19 23:57:24 +0300

    #lambda hands on

---
+++ javatechnologies/src/homework12/lambda.java
@@ -0,0 +1,19 @@
+package homework12;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class lambda extends Application {
+	
+	public static void main(String[] args) {
+		
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
