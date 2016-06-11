53035a59b77d446a6b25ec693161944435797aab
nazmul <nazmul.apu.hasan@gmail.com>
2016-06-11 18:35:47 +0300

    Making a word from form a Sting List item

--- javatechnologies/src/homework12/Word.java
+++ javatechnologies/src/homework12/Word.java
@@ -1,5 +1,23 @@
 package homework12;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Word {
+	
+	public static void main(String[] args) {
+		List<String> words = Arrays.asList("let", "any", "mut", "begin", "do", "after");
+
+		Object[] letter =  words.stream().map(s -> s.charAt(0)).toArray();
+		
+		String word = Arrays.toString(letter)
+				.replace(",", "")
+				.replace("[", "")
+				.replace("]", "")
+				.replace(" ", "")
+				.trim();
+		System.out.println(word);
+		// output lambda
+	}
 
 }
